4c32431a408a7163c7c34cb61acf704d367d2b64
John Ward <ward.johnm@gmail.com>
2015-05-08 13:35:27 -0700

    commented the app

--- js/app.js
+++ js/app.js
@@ -2,8 +2,8 @@ var tttApp = angular.module('tttApp', []);
 
 tttApp.controller('tttController', function() {
   var self = this;
-  self.winner= 'Unkown';
-  self.turns = 0;
+  self.winner= 'Unkown';                                              //Winner Declaration
+  self.turns = 0;                                                     //Turn Counter 
   self.squares =[
     {index:0, value:0}, 
     {index:1, value:0}, 
@@ -15,16 +15,15 @@ tttApp.controller('tttController', function() {
     {index:7, value:0}, 
     {index:8, value:0}
     ];
-  self.reset = self.squares;
-  self.p1Wins = 0;
+  self.p1Wins = 0;                                                  //Counters for Win Display
   self.p2Wins = 0;
-  self.gameOver = false;
-  self.declareWinner = function(x){
+  self.gameOver = false;                                            //Boolean for game state
+  self.declareWinner = function(x){                                 //Function to declare a winner and increment counters
     if(self.squares[x].value==1){
         self.winner = 'Player 1';
         self.p1Wins++;
     }else{self.winner = 'Player 2'; self.p2Wins++;}
-        self.gameOver=true;
+        self.gameOver=true;                                         //locks game board until reset
   }
   self.makeMove = function(x){
   	if(self.squares[x].value == 0 && self.gameOver == false){
@@ -35,28 +34,28 @@ tttApp.controller('tttController', function() {
   	  	}
   	  	if(self.turns>7){self.winner = 'The Cat';
   	  	}else if(self.turns>3){
-  	  			if(self.squares[0].value==self.squares[1].value&&self.squares[1].value==self.squares[2].value && self.squares[0].value!=0){
+  	  			if(self.squares[0].value==self.squares[1].value&&self.squares[1].value==self.squares[2].value && self.squares[0].value!=0){         //row 1
               self.declareWinner(0);
-  	  			}else if(self.squares[3].value==self.squares[4].value&&self.squares[4].value==self.squares[5].value && self.squares[3].value!=0){
+  	  			}else if(self.squares[3].value==self.squares[4].value&&self.squares[4].value==self.squares[5].value && self.squares[3].value!=0){   //row 2
   	  				self.declareWinner(3);
-  	  			}else if(self.squares[6].value==self.squares[7].value&&self.squares[7].value==self.squares[8].value && self.squares[6].value!=0){
+  	  			}else if(self.squares[6].value==self.squares[7].value&&self.squares[7].value==self.squares[8].value && self.squares[6].value!=0){   //row 3
   	  				self.declareWinner(6);
-  	  			}else if(self.squares[0].value==self.squares[3].value&&self.squares[3].value==self.squares[6].value && self.squares[0].value!=0){
+  	  			}else if(self.squares[0].value==self.squares[3].value&&self.squares[3].value==self.squares[6].value && self.squares[0].value!=0){   //column 1
   	  				self.declareWinner(0);
-  	  			}else if(self.squares[1].value==self.squares[4].value&&self.squares[4].value==self.squares[7].value && self.squares[1].value!=0){
+  	  			}else if(self.squares[1].value==self.squares[4].value&&self.squares[4].value==self.squares[7].value && self.squares[1].value!=0){   //column 2 
   	  				self.declareWinner(1);
-  	  			}else if(self.squares[2].value==self.squares[5].value&&self.squares[5].value==self.squares[8].value && self.squares[2].value!=0){
+  	  			}else if(self.squares[2].value==self.squares[5].value&&self.squares[5].value==self.squares[8].value && self.squares[2].value!=0){   //column 3
   	  				self.declareWinner(2);
-  	  			}else if(self.squares[0].value==self.squares[4].value&&self.squares[4].value==self.squares[8].value && self.squares[0].value!=0){
+  	  			}else if(self.squares[0].value==self.squares[4].value&&self.squares[4].value==self.squares[8].value && self.squares[0].value!=0){   //top left to bottom right
   	  				self.declareWinner(0);
-  	  			}else if(self.squares[2].value==self.squares[4].value&&self.squares[4].value==self.squares[6].value && self.squares[2].value!=0){
+  	  			}else if(self.squares[2].value==self.squares[4].value&&self.squares[4].value==self.squares[6].value && self.squares[2].value!=0){   //bottom left to top right
   	  				self.declareWinner(2);
   	  			}
   	  		}
   	  	self.turns++;
   	  }
   	}
-  	self.restart = function(){
+  	self.restart = function(){       //function to reset the game state to origin
   		self.turns = 0;
       self.squares =[
     {index:0, value:0}, 
