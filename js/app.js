d0158d77ac50e7896e7fed7c88517c317deafb06
John Ward <ward.johnm@gmail.com>
2015-05-11 15:34:10 -0700

    unknown changes

--- js/app.js
+++ js/app.js
@@ -3,8 +3,8 @@ var tttApp = angular.module('tttApp', []);
 tttApp.controller('tttController', function() {
   var self = this;
   self.winner= '';                                              //Winner Declaration
-  self.turns = 0;                                                     //Turn Counter 
-  self.squares =[
+  self.turns = 0;                                               //Turn Counter 
+  self.squares=[
     {index:0, value:0}, 
     {index:1, value:0}, 
     {index:2, value:0}, 
@@ -16,8 +16,10 @@ tttApp.controller('tttController', function() {
     {index:8, value:0}
     ];
   self.p1Lines = '';
-  self.p2Lines = '';                                                  //Counters for Win Display
-  self.gameOver = false;                                            //Boolean for game state
+  self.p2Lines = ''; 
+  self.catLines = '';                                                 //Counters for Win Display
+  self.gameOver = false;                                              //Boolean for game state
+  self.cat = false;                                           
 
   self.declareWinner = function(x){                                 //Function to declare a winner and increment counters
     if(self.squares[x].value==1){
@@ -35,6 +37,7 @@ tttApp.controller('tttController', function() {
   	  		self.squares[x].value = 5;
   	  	}
         if(self.turns>3){
+        	for(i=0, i++)
   	  			if(self.squares[0].value==self.squares[1].value&&self.squares[1].value==self.squares[2].value && self.squares[0].value!=0){         //row 1
               self.declareWinner(0);
   	  			}else if(self.squares[3].value==self.squares[4].value&&self.squares[4].value==self.squares[5].value && self.squares[3].value!=0){   //row 2
@@ -51,7 +54,9 @@ tttApp.controller('tttController', function() {
   	  				self.declareWinner(0);
   	  			}else if(self.squares[2].value==self.squares[4].value&&self.squares[4].value==self.squares[6].value && self.squares[2].value!=0){   //bottom left to top right
   	  				self.declareWinner(2);
-  	  			}else if(self.turns>7){self.winner = 'The Cat';
+  	  			}else if(self.turns>7){
+              self.cat = true;
+              self.catLines = self.catLines + '|';
             }
   	  		}
   	  	self.turns++;
@@ -59,7 +64,7 @@ tttApp.controller('tttController', function() {
   	}
   	self.restart = function(){       //function to reset the game state to origin
   		self.turns = 0;
-      self.squares =[
+      self.squares = [
     {index:0, value:0}, 
     {index:1, value:0}, 
     {index:2, value:0}, 
@@ -70,8 +75,16 @@ tttApp.controller('tttController', function() {
     {index:7, value:0}, 
     {index:8, value:0}
     ];
+      console.log(self.squares);
   	self.winner = "  ";
     self.gameOver = false;
+    self.cat = false;
   	}
+    self.reset = function(){
+      self.restart();
+      self.p1Lines = '';
+      self.p2Lines = ''; 
+      self.catLines = '';  
+    }
 
 });
